8a90bb0d6785005bae8c5e1bb48a484cfb3c98a1
vagabondraj <raviraj06112005@gmail.com>
2026-04-21 18:16:43 +0530

    refactor auth controller to include cookie options and add getMe and logout functions

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,9 +1,11 @@
 import constants from '../constants/constant.js';
-import { loginUser, registerUser } from '../services/auth.service.js';
+import { getCurrentUser, loginUser, registerUser } from '../services/auth.service.js';
 import { sendResponse } from '../utils/apiResponse.js';
+import { getAuthCookieOptions } from '../utils/cookie.js';
 
 export const register = async (req, res) => {
   const data = await registerUser(req.body);
+  res.cookie(constants.config.cookieName, data.token, getAuthCookieOptions());
 
   return sendResponse(
     res,
@@ -15,11 +17,33 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   const data = await loginUser(req.body);
+  res.cookie(constants.config.cookieName, data.token, getAuthCookieOptions());
 
   return sendResponse(
     res,
     constants.statusCodes.OK,
     constants.messages.AUTH.LOGIN_SUCCESS,
     data
+  );
+};
+
+export const getMe = async (req, res) => {
+  const user = await getCurrentUser(req.user.id);
+
+  return sendResponse(
+    res,
+    constants.statusCodes.OK,
+    constants.messages.AUTH.CURRENT_USER_FETCHED,
+    user
+  );
+};
+
+export const logout = async (req, res) => {
+  res.clearCookie(constants.config.cookieName, getAuthCookieOptions());
+
+  return sendResponse(
+    res,
+    constants.statusCodes.OK,
+    constants.messages.AUTH.LOGOUT_SUCCESS
   );
 };
