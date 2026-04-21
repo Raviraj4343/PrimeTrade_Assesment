e31b7502f528a94e8cc186c2f05c0aa59b261497
vagabondraj <raviraj06112005@gmail.com>
2026-04-21 18:00:38 +0530

    add task controller with CRUD operations and response handling

---
+++ Backend/src/controllers/task.controller.js
@@ -0,0 +1,34 @@
+import constants from '../constants/constant.js';
+import {
+  createTask,
+  deleteTask,
+  getTaskById,
+  getTasks,
+  updateTask
+} from '../services/task.service.js';
+import { sendResponse } from '../utils/apiResponse.js';
+
+export const createTaskController = async (req, res) => {
+  const task = await createTask(req.body, req.user);
+  return sendResponse(res, constants.statusCodes.CREATED, constants.messages.TASK.CREATED, task);
+};
+
+export const getTasksController = async (req, res) => {
+  const tasks = await getTasks(req.user);
+  return sendResponse(res, constants.statusCodes.OK, constants.messages.TASK.LISTED, tasks);
+};
+
+export const getTaskController = async (req, res) => {
+  const task = await getTaskById(req.params.id, req.user);
+  return sendResponse(res, constants.statusCodes.OK, constants.messages.TASK.FETCHED, task);
+};
+
+export const updateTaskController = async (req, res) => {
+  const task = await updateTask(req.params.id, req.body, req.user);
+  return sendResponse(res, constants.statusCodes.OK, constants.messages.TASK.UPDATED, task);
+};
+
+export const deleteTaskController = async (req, res) => {
+  await deleteTask(req.params.id, req.user);
+  return sendResponse(res, constants.statusCodes.OK, constants.messages.TASK.DELETED);
+};
